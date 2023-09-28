3dec9a2e142a6fb4046b9548362d8588bae22980
bagoviggo <bagoviggo@gmail.com>
2023-09-28 19:08:19 +0000

    Add 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,14 +1,53 @@
-export default class HolbertonCourse {
+class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
-  }
-  get objGetter() {
-    return this.name;
-    return this.length;
-    return this.students;
+    this._name = this._validateString(name, 'name');
+    this._length = this._validateNumber(length, 'length');
+    this._students = this._validateArray(students, 'students');
   }
 
-  set objGetter() {
-    
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = this._validateString(newName, 'name');
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    this._length = this._validateNumber(newLength, 'length');
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    this._students = this._validateArray(newStudents, 'students');
+  }
+
+  static _validateString(value, propertyName) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${propertyName.charAt(0).toUpperCase() + propertyName.slice(1)} must be a string`);
+    }
+    return value;
+  }
+
+  static _validateNumber(value, propertyName) {
+    if (typeof value !== 'number') {
+      throw new TypeError(`${propertyName.charAt(0).toUpperCase() + propertyName.slice(1)} must be a number`);
+    }
+    return value;
+  }
+
+  static _validateArray(value, propertyName) {
+    if (!Array.isArray(value)) {
+      throw new TypeError(`${propertyName.charAt(0).toUpperCase() + propertyName.slice(1)} must be an array`);
+    }
+    return value;
+  }
+}
+export default HolbertonCourse;

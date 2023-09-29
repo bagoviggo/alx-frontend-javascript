39c730ee9a612fa8b2c30947bdd9eb2b4e580220
bagoviggo <bagoviggo@gmail.com>
2023-09-29 22:51:52 +0000

    Add 3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = '';
+    this._name = '';
+
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._validateAndSet('code', newCode);
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._validateAndSet('name', newName);
+  }
+
+  _validateAndSet(property, value) {
+    if (typeof value === 'string') {
+      this['_' + property] = value;
+    } else {
+      throw new TypeError(`${property.charAt(0).toUpperCase() + property.slice(1)} must be a string`);
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
+

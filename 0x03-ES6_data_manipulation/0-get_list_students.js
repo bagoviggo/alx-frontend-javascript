e62c27207b6cee9faab5b5be1d9646f21a4ef452
bagoviggo <bagoviggo@gmail.com>
2023-10-04 01:49:47 +0000

    Add 0-get_list_students.j

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,6 @@
+export default () => [
+  { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
+  { firstName: 'James', id: 2, location: 'Columbia' },
+  { firstName: 'Serena', id: 5, location: 'San Francisco' },
+];
+
